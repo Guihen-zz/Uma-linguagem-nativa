8759b132dc8ef3429ba05d26d37674c2ac2a2fe8
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-04-08 11:25:45 -0300

    Interpretador para a linguagem

--- linguagem.rkt
+++ linguagem.rkt
@@ -2,7 +2,7 @@
 
 (define-type Símbolo-aritmético
   [número (n : number)]
-  [soma (x : Símbolo-aritmético) (y : Símbolo-aritmético)]
+  [adição (x : Símbolo-aritmético) (y : Símbolo-aritmético)]
   [multiplicação (x : Símbolo-aritmético) (y : Símbolo-aritmético)])
 
 (define (analisa [entrada : s-expression]) : Símbolo-aritmético
@@ -11,7 +11,13 @@
     [(s-exp-list? entrada)
      (let ([lista (s-exp->list entrada)])
        (case (s-exp->symbol (first lista))
-         [(+) (soma (analisa (second lista)) (analisa (third lista)))]
+         [(+) (adição (analisa (second lista)) (analisa (third lista)))]
          [(*) (multiplicação (analisa (second lista)) (analisa (third lista)))]
          [else (error 'analisa "A lista dada possui entradas inválida")]))]
      [else (error 'analisa "Entrada inválida")]))
+
+(define (interpreta [expressão : Símbolo-aritmético]) : number
+  (type-case Símbolo-aritmético expressão
+    [número (n) n]
+    [adição (x y) (+ (interpreta x) (interpreta y))]
+    [multiplicação (x y) (* (interpreta x) (interpreta y))]))
