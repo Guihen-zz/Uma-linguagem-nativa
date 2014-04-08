260a9dd03d71364bb2e31287dc6d2de7d4bde2ba
Guilherme Henrique Rojas <guilhermehrojas@gmail.com>
2014-04-08 10:58:02 -0300

    Resolvido erros de sintaxe

--- linguagem.rkt
+++ linguagem.rkt
@@ -2,17 +2,16 @@
 
 (define-type Símbolo-aritmético
   [número (n : number)]
-  [soma (x : SímboloAritmético) (y : SímboloAritmético)]
-  [multiplicação (x : SímboloAritmético) (y : SímboloAritmético)])
+  [soma (x : Símbolo-aritmético) (y : Símbolo-aritmético)]
+  [multiplicação (x : Símbolo-aritmético) (y : Símbolo-aritmético)])
 
 (define (analisa [entrada : s-expression]) : Símbolo-aritmético
   (cond
     [(s-exp-number? entrada) (número (s-exp->number entrada))]
     [(s-exp-list? entrada)
-     (let ([lista (s-exp->list s)])
+     (let ([lista (s-exp->list entrada)])
        (case (s-exp->symbol (first lista))
-         [(+) (soma (analisa (second lista) (analisa (third lista))))]
-         [(*) (multiplicação (analisa (second lista) (analisa (third lista))))]
+         [(+) (soma (analisa (second lista)) (analisa (third lista)))]
+         [(*) (multiplicação (analisa (second lista)) (analisa (third lista)))]
          [else (error 'analisa "A lista dada possui entradas inválida")]))]
      [else (error 'analisa "Entrada inválida")]))
-         
